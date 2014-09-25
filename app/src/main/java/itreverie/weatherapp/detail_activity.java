a26a3f0682f20a6157051834ee25350bf685f6b3
itReverie <brenda.jimenez@itreverie.com>
2014-09-26 08:44:59 +1000

    Adding a ShareActionProvider button.

--- app/src/main/java/itreverie/weatherapp/detail_activity.java
+++ app/src/main/java/itreverie/weatherapp/detail_activity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+
+
+
 
 
 public class detail_activity extends Activity {
@@ -51,30 +59,60 @@ public class detail_activity extends Activity {
      */
     public static class Detail_Fragment extends Fragment {
 
+        private static final String LOG_TAG=Detail_Fragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+        private String mForecastString;
+
         public Detail_Fragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            //View rootView = inflater.inflate(R.layout.detail_fragment, container, false);
-            //return rootView;
-
-            //Get the intent
-            Intent intent= getActivity().getIntent();
 
             //Populate it with the detail fragment XML
             View rootView = inflater.inflate(R.layout.detail_fragment,container, false);
+
+            //Get the intent
+            Intent intent= getActivity().getIntent();
 
             //If the intent is different that null and it has an extra text
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
             {
                 //Write that text in the Text View
-                String forecastStr= intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+                mForecastString= intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastString);
             }
 
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu,MenuInflater inflater)
+        {
+            inflater.inflate(R.menu.detail_share,menu);
+
+            MenuItem menuItem= menu.findItem(R.id.detail_action_share);
+            ShareActionProvider mShareActionProvider=(ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if(mShareActionProvider != null)
+            {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }
+            else
+            {
+                Log.d(LOG_TAG,"Share Action Provider is null?");
+            }
+
+        }
+
+        private Intent createShareForecastIntent()
+        {
+            Intent shareIntent=new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastString+FORECAST_SHARE_HASHTAG);
+            return  shareIntent;
         }
     }
 }
